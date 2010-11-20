83103f7d5f38a9a9ab8355a6eef3d8258756273c
Vojtech Kusy <wojtha@gmail.com>
2010-11-20 20:52:29 +0100

    Adding javascript for disabling custom alias field when pathauto checkbox is checked.

---
+++ nodesymlinks.js
@@ -0,0 +1,26 @@
+// $Id$
+
+Drupal.behaviors.nodereference = function(context){
+
+  $('#nodesymlinks-items tr:not(.ns-processed)', context).each(function(i){
+
+    // Attach checkbox click handler
+    $('#edit-menu-nodesymlinks-items-'+i+'-alias-pathauto').click( function(){
+      // Auto-alias checked; disable input.
+      if ($('#edit-menu-nodesymlinks-items-'+i+'-alias-pathauto').is(':checked')) {
+        $('#edit-menu-nodesymlinks-items-'+i+'-alias-path').attr('disabled', 'disabled');
+      }
+      // Auto-alias unchecked; enable input.
+      else {
+        $('#edit-menu-nodesymlinks-items-'+i+'-alias-path').removeAttr('disabled').focus();
+      }
+    });
+
+    // Disable input
+    if ($('#edit-menu-nodesymlinks-items-'+i+'-alias-pathauto').is(':checked')) {
+      $('#edit-menu-nodesymlinks-items-'+i+'-alias-path').attr('disabled', 'disabled');
+    }
+
+  }).addClass('ns-processed');
+
+};
